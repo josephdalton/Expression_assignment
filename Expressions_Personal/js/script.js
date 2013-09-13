d0c7d57ae39fedc49c1dd1828e779ca45e0eede4
cherrellpoling <cpoling@fullsail.edu>
2013-09-12 22:13:21 -0400

    Testing different variables in the expression to ensure effectivness.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,9 +3,9 @@
 
 //Givens listed below:
 
-var d = 200; //the amount of detergent in a full container at the start of the week
-var l = 15; // the number of laundry loads washed during the week.
-var u = 4; // the fuild ounces required to was a load of clothes. 
+var d = 500; //the amount of detergent in a full container at the start of the week
+var l = 22; // the number of laundry loads washed during the week.
+var u = 10; // the fuild ounces required to was a load of clothes. 
 var text = "fluid ounces remaining after washing clothes for one week "// the dialog for the remaining amount of fuild onces in the full container.
 
 d -= (l * u); // the amount of washed loads is multiplied by the amount of detergent required per load. The amount is subtracted from the full container. 
