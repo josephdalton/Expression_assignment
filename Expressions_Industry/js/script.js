10ad5012538eef2a3ce1b69d4455304bcefd1d00
cherrellpoling <cpoling@fullsail.edu>
2013-09-12 22:26:20 -0400

    Decided to turn the problem into an array, set up the numbers which were the variables in array format.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,6 +2,4 @@
 // How many phone calls do I take throughout the work day?
 //Givens
 
-var workday = 480; // the amount of minutes in an entire work day
-var aht = 20; // the amount of minutes it takes to handle each call
-var hour = 60; // the amount of minutes in an hour
+var minutes = [20,60, 480,]
