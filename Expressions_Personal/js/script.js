c10233de04d5eb5e87a8d786a88f0f0e62f20023
cherrellpoling <cpoling@fullsail.edu>
2013-09-12 22:03:51 -0400

    trying to shorten the expressions using assignment operator, testing now.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,13 +3,12 @@
 
 //Givens
 
-var detergent = 150; //the amount of detergent in a full container at the start of the week
+var d = 150; //the amount of detergent in a full container at the start of the week
 var loads = 50; // the number of laundry loads washed during the week
 var perLoad = 3; // the amount of detergent it takes per load  
 var metric = "fluid ounces remaining after washing clothes for one week "// the dialog for the remaining amount of fuild onces in the full container
 
-
-var remaining = detergent - (loads * perLoad) // the amount of washed loads is multiplied by the amount it takes to wash per load. This amount is subtracted from the starting amount to produce the answer.
+d -= (loads * perLoad); // the amount of washed loads is multiplied by the amount it takes to wash per load. This amount is subtracted from the starting amount to produce the answer.
 
 var answer = String(remaining) + " " +String(metric)// code written so that the string will appear the correct way in the log
 
