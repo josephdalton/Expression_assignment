310f9153f38ece8845e33c66f3a82b4976b657fb
cherrellpoling <cpoling@fullsail.edu>
2013-09-12 22:09:25 -0400

    Testing out different numbers to see how it affects the code.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,9 +3,9 @@
 
 //Givens
 
-var d = 150; //the amount of detergent in a full container at the start of the week
-var l = 50; // the number of laundry loads washed during the week
-var u = 3; // the amount of detergent it takes per load  
+var d = 200; //the amount of detergent in a full container at the start of the week
+var l = 15; // the number of laundry loads washed during the week
+var u = 4; // the amount of detergent it takes per load  
 var metric = "fluid ounces remaining after washing clothes for one week "// the dialog for the remaining amount of fuild onces in the full container
 
 d -= (l * u); // the amount of washed loads is multiplied by the amount it takes to wash per load. This amount is subtracted from the starting amount to produce the answer.
