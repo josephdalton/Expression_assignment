549c174a6c41f9832cd392e07bc726c93c3c3cf0
cherrellpoling <cpoling@fullsail.edu>
2013-09-12 21:06:48 -0400

    Initial set up of the Javascript as well as the index for HTML.

---
+++ Expressions_Personal/script.js
@@ -0,0 +1,9 @@
+//Personal Expression
+//How much laundry detergent is left after washing a weeks worth of clothes?
+
+//Givens
+
+var startingDetergent = 150 fl oz; //the amount of detergent in a full container at the start of the week
+var clothesLoads = 10; // the number of laundry loads washed during the week
+var perLoad = 3 fl oz; // the amount of detergent it takes per load 
+var remainingDetergent
