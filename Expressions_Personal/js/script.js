b4cafbffc9c404e286fb183ec7de6b8fe9ca823f
cherrellpoling <cpoling@fullsail.edu>
2013-09-12 21:47:57 -0400

    Rewrote the expression and included the coding for the answer as well as the metrics that appear after the anwer.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,12 +3,16 @@
 
 //Givens
 
-var startingDetergent = 150 fl oz; //the amount of detergent in a full container at the start of the week
+var startingDetergent = 150; //the amount of detergent in a full container at the start of the week
 var clothesLoads = 10; // the number of laundry loads washed during the week
-var perLoad = 3 fl oz; // the amount of detergent it takes per load 
-var remainingDetergent; // the amount of detergent remaining after the week is over. 
+var perLoad = 3; // the amount of detergent it takes per load  
+var metric = "fluid ounces remaining "// the dialog for the remaining amount of fuild onces in the full container
 
-//the clothes loaded mulitplied by the amount of fluid ounces it will take per load will give us the total amount of detergent needed. Then the answer is then subtracted by the full amount of detergent. 
 
-var amountRemaining = clothesLoads + perLoad
-console.log(amountRemaining)
+var amountRemaining = startingDetergent - (clothesLoads * perLoad) // the amount of washed loads is multiplied by the amount it takes to wash per load. This amount is subtracted from the starting amount to produce the answer.
+
+var answer = String(amountRemaining) + " " +String(metric)// code written so that the string will appear the correct way in the log
+
+console.log(answer)//the answer to the question will be in the log
+
+
