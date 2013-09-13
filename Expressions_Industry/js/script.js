fb36e004f557ed24f7f570d2c721335a57de2380
cherrellpoling <cpoling@fullsail.edu>
2013-09-12 22:37:56 -0400

    Fixed some wording in the notes as well as fixed the amount of the third number in the array because it was wrong. Testing again to see if the correct total comes out.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,9 +2,9 @@
 // How many phone calls do I take throughout the work day?
 //Givens
 
-var minutes = [20,60, 480,] // first number is the average handle time of each call, second is the amount of minutes in an hour, third is the amount of minutes per work shift.
+var minutes = [20,60, 8,] // first number is the average handle time of each call, second is the amount of minutes in an hour, third is the amount of hours worked per shift.
 
-var total = (minutes[0] / minutes[1]) * minutes[2];
+var total = (minutes[0] / minutes[1]) * minutes[2]; //this expression is the amount of minutes in an hour are divided by 20 to find out how many calls an hour. That total is multiplied by 8 for the amount of hours in a shift.  
 
 console.log(total);
 
