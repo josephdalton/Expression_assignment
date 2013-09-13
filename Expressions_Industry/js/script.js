868d6ef0fc0382dd1035feaeee23a0fb59cbca2a
cherrellpoling <cpoling@fullsail.edu>
2013-09-12 22:24:39 -0400

    added comments to each of the givens lines to make it easier to understand.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,6 +2,6 @@
 // How many phone calls do I take throughout the work day?
 //Givens
 
-var workday = 480;
-var aht = 20;
-var hour = 60;
+var workday = 480; // the amount of minutes in an entire work day
+var aht = 20; // the amount of minutes it takes to handle each call
+var hour = 60; // the amount of minutes in an hour
