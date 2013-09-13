6306f5df84ff72cef3c19bed31dbdf55e08781f5
cherrellpoling <cpoling@fullsail.edu>
2013-09-12 23:05:07 -0400

    Added the givens to the code, hours of cpu play, day, month, year.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,10 @@
 // Cherrell Stroud 9/12/13 Wacky Expression
 
 //My wacky expression is how many hours do I spend on the computer a year?
+
+//Given 
+
+var cpu = 10;
+var day= 24;
+var week= 7;
+var month= 12
