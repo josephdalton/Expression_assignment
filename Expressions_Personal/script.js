d2fa4542c29fa25f77528905dec8b77bef9379f2
cherrellpoling <cpoling@fullsail.edu>
2013-09-12 21:17:31 -0400

    Added the expression which is the amount of fluid oz required for each load is multipled by the amount of clothes for the week. That amount is subtracted from the total detergent from the beginning of the week.

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -1,2 +1,14 @@
 //Personal Expression
 //How much laundry detergent is left after washing a weeks worth of clothes?
+
+//Givens
+
+var startingDetergent = 150 fl oz; //the amount of detergent in a full container at the start of the week
+var clothesLoads = 10; // the number of laundry loads washed during the week
+var perLoad = 3 fl oz; // the amount of detergent it takes per load 
+var remainingDetergent; // the amount of detergent remaining after the week is over. 
+
+//the clothes loaded mulitplied by the amount of fluid ounces it will take per load will give us the total amount of detergent needed. Then the answer is then subtracted by the full amount of detergent. 
+
+var amountRemaining = (perLoad * clothesLoads) - startingDetergent;
+console.log(amountRemaining)
